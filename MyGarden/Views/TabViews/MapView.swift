e2863927f9733456f7802a9888348a3afdf08dd3
alcode111 <al.code.111@gmail.com>
2024-11-13 14:36:40 +0100

    Update MapView.swift

--- MyGarden/Views/TabViews/MapView.swift
+++ MyGarden/Views/TabViews/MapView.swift
@@ -6,13 +6,54 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    @State private var position: MapCameraPosition = .userLocation(fallback: .region(
+            MKCoordinateRegion(
+                center: CLLocationCoordinate2D(latitude: 40.83711526053254, longitude: 14.306408393642307), // Example center; replace with your desired fallback
+                span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01) // Adjust zoom level here
+            )
+        ))
+    
+    @State private var isShowingNewPlantModal = false
+    
+    @State private var selectedEmoji = "🌸"
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Map(position: $position) {
+//                Annotation("Flower", coordinate: $position) {
+//                    MapAnnotationView()
+//                }
+            }
+            .mapControls {
+                MapUserLocationButton()
+                MapCompass()
+                MapScaleView()
+            }
+            .overlay(alignment: .bottomTrailing) {
+                Button {
+                    isShowingNewPlantModal.toggle()
+                } label: {
+                    Image(systemName: "leaf.fill")
+                        .font(.title2)
+                        .padding()
+                        .background(.backgroundGreen)
+                        .foregroundStyle(.middleGreen)
+                        .clipShape(Circle())
+                        .padding()
+                }
+            }
+        }
+        .sheet(isPresented: $isShowingNewPlantModal) {
+            NewPlantView()
+        }
     }
 }
 
 #Preview {
     MapView()
 }
+
