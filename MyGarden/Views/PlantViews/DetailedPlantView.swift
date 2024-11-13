8eb5a5b77d99ee496ab708155ead16be0a42918b
enggr-bilal <engr.muhammadbilal97@gmail.com>
2024-11-13 15:16:35 +0100

    add a form and section

--- MyGarden/Views/PlantViews/DetailedPlantView.swift
+++ MyGarden/Views/PlantViews/DetailedPlantView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct DetailedPlantView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section("Take a picture") {
+                    
+                }
+            }
+            .navigationTitle("Add a new plant")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
-
 #Preview {
     DetailedPlantView()
 }
